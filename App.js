3448b710a6a86503004f60f6f6d63c64f301a634
Feiyang_Yang <Feiyang_Yang@dell.com>
2019-05-05 14:31:07 +0800

    add demo for navigation

--- App.js
+++ App.js
@@ -1,49 +1,110 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+/* Follow this page to install the react-navigation & react-native-gesture-handler
+    1. yarn add react-navigation
+      # or with npm
+      # npm install --save react-navigation
+    2. yarn add react-native-gesture-handler
+      # or with npm
+      # npm install --save react-native-gesture-handler
+    3. react-native link react-native-gesture-handler
+    4. Modify MainActivity.java in ./android/app/src/main/java/com/<project>/MainActivity.java
+       The line with + is to add.
+      
+    package com.reactnavigation.example;
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    import com.facebook.react.ReactActivity;
+    + import com.facebook.react.ReactActivityDelegate;
+    + import com.facebook.react.ReactRootView;
+    + import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-type Props = {};
-export default class App extends Component<Props> {
+    public class MainActivity extends ReactActivity {
+
+      @Override
+      protected String getMainComponentName() {
+        return "Example";
+      }
+
+    +  @Override
+    +  protected ReactActivityDelegate createReactActivityDelegate() {
+    +    return new ReactActivityDelegate(this, getMainComponentName()) {
+    +      @Override
+    +      protected ReactRootView createRootView() {
+    +       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+    +      }
+    +    };
+    +  }
+    }
+*/
+
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            /* 1. Navigate to the Details route with params */
+            this.props.navigation.navigate('Details', {
+              itemId: 86,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+class DetailsScreen extends React.Component {
+  render() {
+    /* 2. Get the param, provide a fallback value if not available */
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('itemId', 'NO-ID');
+    const otherParam = navigation.getParam('otherParam', 'some default value');
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>itemId: {JSON.stringify(itemId)}</Text>
+        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() =>
+            this.props.navigation.push('Details', {
+              itemId: Math.floor(Math.random() * 100),
+            })}
+        />
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
