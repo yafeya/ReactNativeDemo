26b0cf3fa492936123dd8e913de321e42ccf7a29
Feiyang_Yang <Feiyang_Yang@dell.com>
2019-05-05 13:50:28 +0800

    add state & props demo

--- App.js
+++ App.js
@@ -7,43 +7,48 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+class InputForm extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  onEndEditing(e) { 
+    let text = e.nativeEvent.text;
+    this.props.onTextChanged(text);
+    this.setState({ text: `The input: ${text}` });
+
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Type here to translate!"
+          onEndEditing={(e) => this.onEndEditing(e)}
+        />
+        <Text>{this.state.text}</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class App extends Component {
+
+  onTextChanged(text) { 
+    alert(`The input is ${text}`);
+  }
+
+  render() {
+    return (
+      <View>
+        <InputForm onTextChanged={(text) => this.onTextChanged(text)} />
+      </View>
+    );
+  }
+}
+
